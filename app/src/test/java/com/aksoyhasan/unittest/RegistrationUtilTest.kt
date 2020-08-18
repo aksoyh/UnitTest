7c5c30c7cb37bc100f8300264318e27e58ea6271
aksoyh <aksoy.android@gmail.com>
2020-08-18 18:43:16 +0300

    Initial commit

---
+++ app/src/test/java/com/aksoyhasan/unittest/RegistrationUtilTest.kt
@@ -0,0 +1,68 @@
+package com.aksoyhasan.unittest
+
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class RegistrationUtilTest {
+
+    @Test
+    fun `empty username returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "furkan",
+            "1234567",
+            "1234567"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "caner",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirmed password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "hasan",
+            "123456",
+            "abcdefg"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "koray",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `less than 6 digit password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "hasan",
+            "abcdefg5",
+            "abcdefg5"
+        )
+        assertThat(result).isFalse()
+    }
+}
